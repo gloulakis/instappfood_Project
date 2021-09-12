910ba8062abffeb024b5ba36c6b7e6d9cc59cce1
George <51861795+gloulakis@users.noreply.github.com>
2021-09-12 23:50:39 +0300

    Changes

--- App.js
+++ App.js
@@ -15,6 +15,8 @@ const store = createStore(rootReducer,applyMiddleware(thunk))
 import LandingScreen from './components/auth/Landing'
 import RegisterScreen from './components/auth/Register'
 import MainScreen from './components/Main'
+import AddScreen from './components/main/Add'
+import SaveScreen from './components/main/Save'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDq9NKaC2mkb9dlBiAVlY_cVHiuBBucAXE",
@@ -80,7 +82,13 @@ export class App extends Component {
     }
     return(
       <Provider store={store}>
-          <MainScreen/>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Main">
+            <Stack.Screen name ="Main" component = { MainScreen} options = {{headerShown: false}}/>
+            <Stack.Screen name ="Add" component = { AddScreen} navigation={this.props.navigation}/>
+            <Stack.Screen name ="Save" component = { SaveScreen}/>
+          </Stack.Navigator>
+        </NavigationContainer>
       </Provider>
       
     )
--- components/Main.js
+++ components/Main.js
@@ -1,26 +1,60 @@
 import React, { Component } from 'react'
 import { View,Text } from 'react-native'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import {connect} from 'react-redux'
 import { bindActionCreators } from 'redux'
-import {fetchUser} from '../redux/actions/index'
+import {fetchUser,fetchUserPosts} from '../redux/actions/index'
+import FeedScreen from './main/Feed'
+import ProfileScreen from './main/Profile'
 
+const Tab = createMaterialBottomTabNavigator();
+const EmptyScreen = () =>{
+    return(null)
+}
 export class Main extends Component {
     componentDidMount(){
-
+        this.props.fetchUser();
+        this.props.fetchUserPosts();
     }
     render() {
         return (
-            <view style={{flex: 1, justifyContent:'center'}}>
-                <Text>
-                    user is logged in
-                </Text>
-            </view>
+            <Tab.Navigator initialRouteName="Feed" labeled={false}>
+                <Tab.Screen name="Feed" component={FeedScreen} 
+                options = {{
+                    tabBarIcon: ({color,size}) =>(
+                        <MaterialCommunityIcons name = "home" color = {color} size = {26}/>
+                    ),
+                }}/>
 
+                   <Tab.Screen name="Add Info" component={EmptyScreen} 
+                   listeners ={({navigation})=>
+                ({
+                    tabPress: event =>{
+                        event.preventDefault();
+                        navigation.navigate("Add")
+                    }
+                })}
+                options = {{
+                    tabBarIcon: ({color,size}) =>(
+                        <MaterialCommunityIcons name = "plus-box" color = {color} size = {26}/>
+                    ),
+                }}/>
+
+                  <Tab.Screen name="Profile" component={ProfileScreen} 
+                options = {{
+                    tabBarIcon: ({color,size}) =>(
+                        <MaterialCommunityIcons name = "account-circle" color = {color} size = {26}/>
+                    ),
+                }}/>
+            </Tab.Navigator>
         )
     }
 }
 
-
-
-export default Main
+const mapStateToProps = (store) =>({
+    currentUser : store.userState.currentUser
+})
+const mapDispatchProps = (dispatch) => bindActionCreators({fetchUser,fetchUserPosts},dispatch)
+export default connect(mapStateToProps,mapDispatchProps)(Main)
