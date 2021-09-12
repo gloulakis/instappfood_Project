f94dfbacbeb67a889b4d67992700b92fa1335779
George <51861795+gloulakis@users.noreply.github.com>
2021-09-12 23:51:08 +0300

    changes

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,20 +1,45 @@
-import { USER_STATE_CHANGE } from "../constants/index";
-
+import { USER_STATE_CHANGE,USER_POSTS_STATE_CHANGE } from "../constants/index";
 import firebase from "firebase";
 
 export function fetchUser(){
-    return((dispatch)=>
+    return((dispatch)=> {
     firebase.firestore()
-        .collection("user")
+        .collection("users")
         .doc(firebase.auth().currentUser.uid)
         .get()
         .then((snapshot)=>{
             if (snapshot.exists){
                 dispatch({type:USER_STATE_CHANGE,currentUser:snapshot.data()})
+
             }else{
                 console.log('does not exist')
             }
-        }
-        )  
-    )
+        })  
+    })
+}
+
+export function fetchUserPosts() {
+    return ((dispatch) => {
+        firebase.firestore()
+            .collection("posts")
+            .doc(firebase.auth().currentUser.uid)
+            .collection("userPosts")
+            .orderBy("creation", "asc")
+            .get()
+            .then((snapshot) => {
+            if (snapshot.exists){
+                let posts = snapshot.docs.map(doc => {
+                    const data = doc.data();
+                    const id = doc.id;
+                    return { id, ...data }
+                })
+                dispatch({ type: USER_POSTS_STATE_CHANGE, posts })
+                console.log(posts)
+            } else{
+                console.log('Post Error!!')
+            }
+
+           
+        })  
+    })
 }
